5efbc0fe3d57e109e05a4e93215cc6c298d4a6ba
rohit singh <155946618+rohittt04@users.noreply.github.com>
2025-06-28 18:02:50 +0530

    Loops excersice is added

---
+++ Loops.js
@@ -0,0 +1,42 @@
+// simple tasks 
+// while loop
+// example 1
+let sum = 0;
+let i = 1;
+while (i <= 5) {
+    sum = sum + i;
+    i++;
+}
+// console.log(sum);
+
+
+// example 2 store number in array from 5 to 1 
+let countdown = [];
+let j = 5;
+while (j > 0) {
+    countdown.push(j);
+    j--;
+}
+// console.log(countdown);
+
+
+// Do-While Loop
+
+// example 3 take user input until user says stop
+let teaCollection = [];
+let tea;
+// do {
+//     tea = prompt(`Enter your favourate tea name (type "stop" to finish): `)
+//     if (tea !== "stop")
+//         teaCollection.push(tea)
+
+// } while (tea !== "stop");
+
+//example 4 make a array [2,4,6] now run a for loop that multiplies each element of array Number with 2 and store them into another array  multipliednumber 
+let multipliedNumber = [];
+let Number = [2, 4, 6]; 
+for (let l = 0; l < Number.length; l++) {
+    multipliedNumber.push(Number[l]*2);   
+}
+// console.log(multipliedNumber);
+
