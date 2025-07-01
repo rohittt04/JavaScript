a9fbb76576de5371a690ae5abde20d64ef6daa28
rohit singh <155946618+rohittt04@users.noreply.github.com>
2025-07-01 17:35:28 +0530

    oop folder is added with new files

---
+++ oop/oop-master.js
@@ -0,0 +1,61 @@
+/* let Car = {
+    model: 2020,
+    color: "black",
+    company: "tata",
+    start: function () {
+        return `${this.company} is started in year ${this.model}`
+    },
+};
+ console.log(Car.start()); // this will throw "tata is started in year 2020" */
+
+function Persion(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+let vivek = new Persion("vivek", 21)
+/* console.log(vivek.age); // this will throw "21" here because i'm accesing it's age here */
+
+
+
+// prototype and prototypal-chain
+
+function Animal(type) {
+    this.type = type;
+}
+
+Animal.prototype.speak = function () {
+    return `${this.type} is makes a sound`
+
+}
+
+let dog = new Animal("dog")
+// console.log(dog.speak());
+
+
+
+// Class
+
+class Vehical {
+    constructor(model , make ) {
+        this.model = model;
+        this.make = make;
+    }
+
+    start(){
+        return `${this.model} has a model  ${this.make} `
+    }
+}
+
+// inheritance 
+
+class Car extends Vehical {
+    drive(){
+        return `${this.make} : this is an inharitance example. `; 
+    }
+}
+
+let myCar = new Car("toyota" , "hylex")
+console.log(myCar.start());
+console.log(myCar.drive());
+
