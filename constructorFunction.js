ba1c6ef5244eccf1622872640e9ad5fc811d8327
rohit singh <155946618+rohittt04@users.noreply.github.com>
2025-07-01 09:51:42 +0530

    prototypes with functions is added

--- constructorFunction.js
+++ constructorFunction.js
@@ -1,16 +1,54 @@
 // constructer function
 
 function Car(name, color, model){
-    this.name = name
+    this.name = name;
     this.color = color;
     this.model = model;
 }
 
 // creating an Object / instance
-let Mycar = new Car("tata", "black", 2022) 
-console.log(Mycar);
+let Mycar = new Car("tata", "black", 2022);
+// console.log(Mycar);
 // out put will be "Car { name: 'tata', color: 'black', model: 2022 }"
 
-let myNewCar = new Car("totyota", "red", 2025)
-console.log(myNewCar);
+let myNewCar = new Car("totyota", "red", 2025);
+// console.log(myNewCar);
 // out put will be "Car { name: 'totyota', color: 'red', model: 2025 }"
+
+// function in function / nested function
+
+function Tea(type){
+    this.type = type;
+    this.describe = function () {
+        return `here is your cup of ${this.type}`;
+        
+    }
+}
+
+let LemonTea = new Tea("lemon tea")
+// console.log(LemonTea.describe());
+
+// Prototypes 
+
+function Animal(species){
+    this.species = species;
+}
+
+Animal.prototype.sound = function(){
+     return `${this.species} makes a sound`
+}
+
+let dog = new Animal("dog")
+console.log(dog.sound());
+
+let cat = new Animal("cat")
+console.log(cat.sound());
+
+
+
+
+
+
+
+
+
