d4b502dc262c126f6c9e6cca58eecd26536d1a45
rohit singh <155946618+rohittt04@users.noreply.github.com>
2025-07-10 11:16:50 +0530

    Dom mainpulation is added in advance folder

---
+++ Advance/script.js
@@ -0,0 +1,19 @@
+// eg 1
+document.getElementById("changeTextButton").addEventListener('click', function() {
+    let paragrph = document.getElementById("MyParagraph");
+    paragrph.textContent = "the paragraph is changed"
+    
+})
+
+//eg 2
+
+document.getElementById("HighLightFirstCity").addEventListener('click', function(){
+    let CityList = document.getElementById("citiesList");
+    CityList.firstElementChild.classList.add('highlight')
+});
+
+// eg 3
+document.getElementById("ChangeOrder").addEventListener('click', function(){
+    let CoffeeType  = document.getElementById("CoffeeType");
+    CoffeeType.textContent = "Espresso";
+})
