0d539c802937c6913ae3b3f57cdc1a9efaef4593
rohit singh <155946618+rohittt04@users.noreply.github.com>
2025-07-17 10:10:58 +0530

    Added  Asynchronous.js file

---
+++ Advance/As_script.js
@@ -0,0 +1,16 @@
+function SayHello(){
+    console.log("hello!!");
+    
+}
+
+setTimeout(() => {
+    SayHello();
+},4000)
+
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+    
+}
+
+// closures in js
+
