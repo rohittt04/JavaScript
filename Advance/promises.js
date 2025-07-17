aa0e9362587c227c2c766a376185304fa6f06342
rohit singh <155946618+rohittt04@users.noreply.github.com>
2025-07-17 13:49:23 +0530

    promises.js file is added

---
+++ Advance/promises.js
@@ -0,0 +1,17 @@
+function FatchData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let success = true;
+            if (success) {
+                resolve("data fetched successfully!!");
+            } else {
+                reject("Error fatching Data!!");
+            }
+        }, 3000);
+    });
+}
+
+//how to consume promis:
+FatchData()
+    .then((data) => console.log(data))
+    .catch((error)=> console.log(error))
