eece74f0426f849ad24f074bc76af6306fedb74f
rohit singh <155946618+rohittt04@users.noreply.github.com>
2025-07-17 10:48:20 +0530

    closers added

--- Advance/As_script.js
+++ Advance/As_script.js
@@ -1,16 +1,25 @@
-function SayHello(){
+function SayHello() {
     console.log("hello!!");
-    
+
 }
 
 setTimeout(() => {
-    SayHello();
-},4000)
+    // SayHello();
+}, 4000)
 
 for (let index = 0; index < 10; index++) {
-    console.log(index);
-    
+    // console.log(index);
+
 }
 
 // closures in js
+function outer() {
+    let counter = 4;
+    return function () {
+        counter++;
+        return counter; // return is the closer here
+    };
+}
 
+let increment = outer()
+console.log(increment());
